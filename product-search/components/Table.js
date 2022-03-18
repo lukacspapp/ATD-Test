ae3b432e290d5cd30fe5dcf9f5997dcffaa5c8fb
lukacspapp <papplukacs@hotmail.com>
2022-03-18 12:01:55 +0000

    styling

--- product-search/components/Table.js
+++ product-search/components/Table.js
@@ -22,9 +22,9 @@ export default function Table() {
 
 
   return (
-    <section class="flex-wrap antialiased justify-center  bg-gray-100 text-gray-600 h-screen px-4">
-      <div class="flex flex-wrap flex-col align-middle justify-center h-full">
-        <div class="w-full max-w-6xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
+    <section class="flex-wrap antialiased justify-center  bg-gray-100 text-gray-600 h-screen px-4 max-h-full">
+      <div class="flex flex-wrap flex-col align-middle justify-center max-h-full">
+        <div class="w-full max-w-6xl mx-auto bg-white shadow-lg rounded-sm border m-6 border-gray-200 max-h-full">
             <header class="flex items-center justify-between px-5 py-4 border-b border-gray-100">
                 <h2 class="font-semibold text-gray-800">Trips and Attractions</h2>
                 <SearchBar />
@@ -55,7 +55,7 @@ export default function Table() {
                             <tr>
                                 <td class="p-2 whitespace-nowrap">
                                     <div class="flex items-center">
-                                        <div class="w-20 h-20 object-fill flex-shrink-0 mr-2 sm:mr-3"><img class="h-20 w-6w rounded-lg" src={trip.img_sml} width="150" height="70" alt="Alex Shatov"/></div>
+                                        <div class="w-20 h-20 object-fill flex-shrink-0 mr-2 sm:mr-3"><img class="h-20 w-60 rounded-lg" src={trip.img_sml} width="15" height="70" alt="Alex Shatov"/></div>
                                     </div>
                                 </td>
                                 <td class="p-2 whitespace-nowrap">
